0e67ae4d3c6d745a90d0704e84091d3e451adaa8
Hussain Ahmed Shrabon <83140185+iamhussaina@users.noreply.github.com>
2025-11-13 21:23:00 +0600

    Create team-member-section.php

---
+++ patterns/team-member-section.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Team Member Section Pattern Markup.
+ *
+ * This file is included by hussainas_get_pattern_markup()
+ * It should ONLY contain the WordPress block markup.
+ *
+ * @package HussainasBlockPatternModule
+ */
+?>
+
+<div class="wp-block-columns alignwide" style="block-gap:2.5rem 2.5rem">
+  <div class="wp-block-column">
+    <figure class="wp-block-image aligncenter size-thumbnail is-resized is-style-rounded" style="border-radius:50%"><img
+        src="https://via.placeholder.com/150" alt="Placeholder image for a team member" width="150" height="150" />
+    </figure>
+    <h4 class="has-text-align-center" style="font-style:normal;font-weight:600">Jane Doe</h4>
+    <p class="has-text-align-center has-primary-color has-text-color" style="font-size:0.9rem">Project Manager</p>
+  </div>
+
+  <div class="wp-block-column">
+    <figure class="wp-block-image aligncenter size-thumbnail is-resized is-style-rounded" style="border-radius:50%"><img
+        src="https://via.placeholder.com/150" alt="Placeholder image for a team member" width="150" height="150" />
+    </figure>
+    <h4 class="has-text-align-center" style="font-style:normal;font-weight:600">John Smith</h4>
+    <p class="has-text-align-center has-primary-color has-text-color" style="font-size:0.9rem">Lead Developer</p>
+  </div>
+
+  <div class="wp-block-column">
+    <figure class="wp-block-image aligncenter size-thumbnail is-resized is-style-rounded" style="border-radius:50%"><img
+        src="https://via.placeholder.com/150" alt="Placeholder image for a team member" width="150" height="150" />
+    </figure>
+    <h4 class="has-text-align-center" style="font-style:normal;font-weight:600">Alex Johnson</h4>
+    <p class="has-text-align-center has-primary-color has-text-color" style="font-size:0.9rem">UX/UI Designer</p>
+  </div>
+</div>
